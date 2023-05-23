35b8be28e02cab314d8afda3e5421237a3da6543
Christian Reiner <foss@christian-reiner.info>
2023-05-23 11:07:56 +0100

    refactor specification of required permissions for bluetooth widgets

---
+++ app/src/main/kotlin/org/rustygnome/gadgetcontrolwidgets/widget/bluetooth/Permissions.kt
@@ -0,0 +1,3 @@
+package org.rustygnome.gadgetcontrolwidgets.widget.bluetooth
+
+const val REQUIRED_PERMISSIONS: String = android.Manifest.permission.BLUETOOTH_CONNECT
--- app/src/main/kotlin/org/rustygnome/gadgetcontrolwidgets/widget/bluetooth/Tag.kt
+++
@@ -1,5 +0,0 @@
-package org.rustygnome.gadgetcontrolwidgets.widget.bluetooth
-
-internal const val TAG = "BluetoothWidget"
-
-const val REQUIRED_PERMISSIONS = android.Manifest.permission.BLUETOOTH_CONNECT
